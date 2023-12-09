caf692e8310369ef0e61ed30dff755b7fedbd7bc
Krasnorutskiy Kirill <dullsharpz@gmail.com>
2023-12-09 14:01:21 +0300

    fix: README, about me

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
-Дипломная работа
+
+# Дипломная работа "Movies app" Яндекс.Практикум (frontend)
+
+Задача frontend части дипломной работы заключалась в верстке адаптивного приложения по макету и реализация его функцонала с помощью JS и React
+
+
+## Features
+
+- Регистрация/Авторизация
+- Валидация полей ввода
+- Редактирование профиля
+- Работа с Api
+- Добавление(лайк) карточек фильмов в коллекцию и удаление из неё
+- Поиск по фильмам / чекбокс для корткометражек
+- Сохранение данных последнего поиска в локальном хранилище
+- Сайт адаптирован для экранов ПК, планшетов и мобильных экранов
+
+
+
+
+## Tech Stack
+
+React, JS, CSS, HTML
+
+
+
+## Demo
 
 Ссылка на рабочий сайт: https://red-movies.nomoredomainsrocks.ru
 
-Ссылка на pull request: https://github.com/Red-Handed-Guy/movies-explorer-frontend/pull/2
 
-Репозиторий https://github.com/Red-Handed-Guy/movies-explorer-frontend
+## Related
 
-Макет **dark-2** https://www.figma.com/file/6FMWkB94wE7KTkcCgUXtnC/light-1?type=design&node-id=891-3857&mode=design&t=2o1n0jVrlmCiWxAF-0
+[Backend](https://github.com/Red-Handed-Guy/movies-explorer-api) часть приложения
+
+[beatfilm-movies Api](https://api.nomoreparties.co/beatfilm-movies)
+
+[Макет](https://www.figma.com/file/6FMWkB94wE7KTkcCgUXtnC/light-1?type=design&node-id=891-3857&mode=design&t=2o1n0jVrlmCiWxAF-0) dark-2 
--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,27 +3,33 @@ import Portfolio from '../Portfolio/Portfolio'
 
 export default function AboutMe() {
   return (
-    <section className="about-me" id="about-me">
-      <h2 className="about-me__title section-title">Студент</h2>
-      <div className="about-me__columns">
-        <div className="about-me__column">
-          <h3 className="about-me__column-title">Кирилл</h3>
-          <p className="about-me__column-subtitle">Фронтенд-разработчик, 30 лет</p>
-          <p className="about-me__column-text">
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь.
-            Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал
-            в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься
-            фриланс-заказами и ушёл с постоянной работы.
+    <section className='about-me' id='about-me'>
+      <h2 className='about-me__title section-title'>Студент</h2>
+      <div className='about-me__columns'>
+        <div className='about-me__column'>
+          <h3 className='about-me__column-title'>Кирилл</h3>
+          <p className='about-me__column-subtitle'>
+            Фронтенд-разработчик, 30 лет
+          </p>
+          <p className='about-me__column-text'>
+            Я родился и живу в г.Одинцово МО, закончил факультет "Городского
+            кадастра" в ГУЗ. Больше 5 лет работал инженером-фотограмметристом в
+            "НИИ ТП". Увлекся сначала версткой сайтов, а затем и
+            программированием на JS из-за того, что на предыдущем месте работы
+            мне не хватало возможности сразу видеть результат своей работы и
+            процесса решения задач. Для более структурированного и
+            продуктивного обучения закончил курс по Web-разработке от Я.Практикума. Участвовал в Хакатонах от Яндекс. Сейчас активно ищу работу в сфере Frontend
+            разработки.
           </p>
           <a
-            className="about-me__column-git link"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/Red-Handed-Guy">
+            className='about-me__column-git link'
+            target='_blank'
+            rel='noreferrer'
+            href='https://github.com/Red-Handed-Guy'>
             Github
           </a>
         </div>
-        <img className="about-me__avatar" src={myAvatar} alt="Фото автора" />
+        <img className='about-me__avatar' src={myAvatar} alt='Фото автора' />
       </div>
       <Portfolio />
     </section>
